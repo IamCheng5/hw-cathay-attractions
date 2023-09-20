d7debaef3318a386625f106501b91eac8778965f
IamCheng5 <b2468623437132@gmail.com>
2023-09-20 13:49:23 +0800

    init

---
+++ app/src/main/java/com/andyliu/attractions/attractions/hw/cathay/attractions/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.andyliu.attractions.attractions.hw.cathay.attractions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.andyliu.attractions.attractions.hw.cathay.attractions.ui.theme.HwcathayattractionsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HwcathayattractionsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HwcathayattractionsTheme {
+        Greeting("Android")
+    }
+}
